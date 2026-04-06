246cf245a4395f81aa4c703a3f6986c0377200ee
Tiago <webdtiago@gmail.com>
2026-04-06 14:59:18 -0300

    update: mobile menu + header premium fix

--- js/script.js
+++ js/script.js
@@ -964,30 +964,6 @@ if (profileBtn && profileMenu && profileOverlay) {
   });
 
 }
-
-
-// =====================
-// MENU MOBILE
-// ====================
-
-const cursor = document.getElementById("cursor");
-
-if (cursor) {
-  document.addEventListener("mousemove", (e) => {
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-  });
-
-  document.querySelectorAll("a, button").forEach(el => {
-    el.addEventListener("mouseenter", () => {
-      cursor.classList.add("big");
-    });
-
-    el.addEventListener("mouseleave", () => {
-      cursor.classList.remove("big");
-    });
-  });
-}
 // =====================
 // MENU MOBILE PREMIUM
 // =====================
